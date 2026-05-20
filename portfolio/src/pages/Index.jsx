a0e1eceacf8148d952a62491fe54732623c90f74
James Corino <122735477+Jamescorino8@users.noreply.github.com>
2026-05-20 12:09:29 -0400

    update index page with internship information

--- portfolio/src/pages/Index.jsx
+++ portfolio/src/pages/Index.jsx
@@ -86,7 +86,7 @@ export default function Index() {
           <p className="stagger-item mb-1">20 yo.</p>
           <p className="stagger-item mb-1">i study computer science.</p>
           <p className="stagger-item mb-1">im most interested in software development.</p>
-          {/* <p className="stagger-item mb-1">currently </p> */}
+          <p className="stagger-item mb-1">incoming research intern in ai for social good @ skku.</p>
         </div>
       </section>
 
